893e0185be9d7e46416ead336338600dc2ad51f1
LaJiOJ <41719159+LaJiOJ@users.noreply.github.com>
2019-06-28 15:50:05 +0800

    Update test.cpp

--- Sophomore Spring/test.cpp
+++ Sophomore Spring/test.cpp
@@ -1,1 +1,7 @@
-
+#include <stdio.h>
+int main(){
+  printf("Hello World!\n");
+  printf("This is just a try.\n");
+  printf("I will figure out how to push new files in to this repository correctly.\n");
+  return 0;
+}
