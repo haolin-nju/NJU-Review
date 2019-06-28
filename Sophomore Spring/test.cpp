9a340b8674a45c4a76bf4ae974326085f7085ff3
LaJiOJ <41719159+LaJiOJ@users.noreply.github.com>
2019-06-28 18:28:35 +0800

    Delete test.cpp

--- Sophomore Spring/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-  printf("Hello World!\n");
-  printf("This is just a try.\n");
-  printf("I will figure out how to push new files in to this repository correctly.\n");
-  return 0;
-}
